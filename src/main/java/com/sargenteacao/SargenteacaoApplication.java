e5a772b7f18901532ac1c566367954eac9b156e4
guto1906 <guto1906@gmail.com>
2019-04-10 22:05:20 -0300

    primeiro commit

---
+++ src/main/java/com/sargenteacao/SargenteacaoApplication.java
@@ -0,0 +1,13 @@
+package com.sargenteacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SargenteacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SargenteacaoApplication.class, args);
+	}
+
+}
